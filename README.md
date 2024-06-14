5fb629b0a17b380f5bbea6f37d2c78f8b04d2f73
dayn9t <dayn9t@gmail.com>
2024-06-14 22:56:07 +0800

    24-06-14

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 海康排他网络(Hik Exclusive Network)
 
+## Rake 
 Rake 参见: https://github.com/dayn9t/rake.
 这是 Rake 项目的一个模板.
 ```
@@ -9,3 +10,11 @@ net
 sed -i 's/hennet/<MOD_A>/g' `rg -l hennet`
 
 ```
+
+## 音频转码
+
+最具性价比的是 mp3 格式, aac 必须 32bit 32k, 大很多
+- G.711 A-law 转 wav: ```ffmpeg -f s16le -acodec pcm_alaw -ar 8000 -ac 1 -i input.raw output.wav```
+    - ***注意input要放在格式之后***
+- G.711 A-law 转 wav: ```ffmpeg -f s16le -acodec pcm_alaw -ar 8000 -ac 1 -i input.raw -ar 16000 output.mp3```
+    - 需要加是16K采样率,否则有杂音
---
+++ apps/hen-download/main.cpp
@@ -0,0 +1,114 @@
+#include <map>
+#include <sstream>
+#include <thread>
+#include <fmt/format.h>
+#include <hen/net/session.hpp>
+#include <hen/net/playback.hpp>
+
+using namespace hen;
+using namespace std;
+
+#include <boost/url.hpp>
+#include <iostream>
+#include <string>
+
+/// 获取查询字符串参数
+map<string, string> get_params(const boost::url& url)
+{
+    map<string, string> query_map;
+
+    for (auto p : url.params())
+    {
+        query_map[p.key] = p.value;
+    }
+
+    return query_map;
+}
+
+/// 录像片段信息
+struct RecordInfo
+{
+    int channel;
+    int stream;
+    int media_type;
+    DatetimeMember start;
+    DatetimeMember end;
+};
+
+
+/// 解析录像记录
+RecordInfo parse_record_info(const boost::url& url)
+{
+    int n = 0;
+    auto p = url.path().c_str();
+    cr_ensure(sscanf(url.path().c_str(), "/record/%d", &n) == 1);
+    auto params = get_params(url);
+
+    RecordInfo info = {};
+
+    info.channel = n / 100;
+    info.stream = n % 100;
+    cr_ensure(parse(params["start_time"], info.start));
+    cr_ensure(parse(params["end_time"], info.end));
+
+    return info;
+}
+
+
+/// 输出URI的各个组件
+void show_parts(const boost::url& url)
+{
+    fmt::println("Scheme: {}", url.scheme());
+    fmt::println("User: {}", url.user());
+    fmt::println("Password: {}", url.password());
+    fmt::println("Host: {}", url.host());
+    fmt::println("Port: {}", url.port());
+    fmt::println("Path: {}", url.path());
+    fmt::println("Query: {}", url.query());
+    //fmt::println("Params: {}", url.params());
+    fmt::println("Fragment: {}", url.fragment());
+}
+
+// "hikn://admin:pass@10.1.0.21:8000/record/101?start_time=2024-05-17T17:07:00&end_time=2024-05-17T17:09:00";
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        fmt::println("Usage:\n\then-download <url>");
+    }
+    boost::url url = boost::url(argv[1]);
+    show_parts(url);
+
+    auto record_info = parse_record_info(url);
+
+
+    NetSDK sdk;
+
+    auto version = sdk.version();
+    fmt::println("Version: {}", version_string(version));
+
+    sdk.set_log(3, "/tmp/hen-app.log");
+
+    Session session("10.1.0.21", 8000, "admin", "howell1409");
+    auto d = session.device_info();
+    fmt::println("Device serial: {}", d.serial_number);
+    fmt::println("  disk number: {}", d.disk_num);
+
+    const DatetimeMember start = {2024, 5, 17, 17, 7, 0};
+    const DatetimeMember end = {2024, 5, 17, 17, 9, 0};
+    const PlaybackInfo info = {1, 0, 1, start, end}; // stream 无影响
+    Playback playback(session.id(), info);
+
+    playback.set_audio_path("/tmp/audio.raw");
+    playback.start();
+
+    while (!playback.done())
+    {
+        std::this_thread::sleep_for(1s);
+        auto len = playback.audio_size();
+        fmt::println("Audio len: {}", len);
+    }
+
+    return 0;
+}
