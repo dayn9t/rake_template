fda440b5298d6a5f4244c41b29c024042e7a6cbe
dayn9t <dayn9t@gmail.com>
2024-07-31 21:30:00 +0800

    24-07-31

--- modules/reader/src/downloader.cpp
+++ modules/reader/src/downloader.cpp
@@ -7,21 +7,25 @@ using namespace std;
 
 CrError hen_downloader_create(const char* src_url, const char* dst_file, HenDownloader* handle)
 {
-    auto ob = new Downloader(src_url, dst_file);
-    *handle = cr_handle_cast(ob, HenDownloader);
-    return CRE_OK;
+    return cr::catch_error([&]
+    {
+        auto ob = new Downloader(src_url, dst_file);
+        *handle = cr_handle_cast(ob, HenDownloader);
+    });
 }
 
 CrError hen_downloader_destroy(HenDownloader handle)
 {
-    auto ob = cr_object_cast(handle, Downloader);
-    delete ob;
-    return CRE_OK;
+    return cr::catch_error([&]{
+        auto ob = cr_object_cast(handle, Downloader);
+        delete ob;
+    });
 }
 
 CrError hen_downloader_transfer(HenDownloader handle, U32* size)
 {
-    auto ob = cr_object_cast(handle, Downloader);
-    *size = U32(ob->transfer());
-    return CRE_OK;
+    return cr::catch_error([&]{
+        auto ob = cr_object_cast(handle, Downloader);
+        *size = U32(ob->transfer());
+    });
 }
